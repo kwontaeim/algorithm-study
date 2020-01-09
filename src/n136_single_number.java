c4771dbaef8cd233babadeb078d6d43d06008a11
Taeim Kwon <k.taemee@gmail.com>
2020-01-09 21:53:30 +0000

    Algorighm problem

--- src/n136_single_number.java
+++ src/n136_single_number.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
 
 public class n136_single_number {
+
+    public static void main(String[] args) {
+
+        int[] nums = {4,1,2,1,2};
+
+        int output = singleNumber(nums);
+        System.out.println(output);
+    }
+
+    public static int singleNumber(int[] nums) {
+
+        HashSet<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (set.contains(nums[i])) {
+                set.remove(nums[i]);
+            } else {
+                set.add(nums[i]);
+            }
+        }
+
+        for (int i : set) {
+            return i;
+        }
+        return -1;
+    }
 }
--- src/n328_odd_even_linked_list.java
+++ src/n328_odd_even_linked_list.java
@@ -1,4 +1,46 @@
-package PACKAGE_NAME;
-
 public class n328_odd_even_linked_list {
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        head = oddEvenList(head);
+
+        ListNode print = head;
+        while (print != null){
+            System.out.print(print.toString());
+            System.out.print("->");
+            print = print.next;
+        }
+
+    }
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+
+        @Override
+        public String toString() {
+            return String.valueOf(val);
+        }
+    }
+
+    public static ListNode oddEvenList(ListNode head) {
+
+        ListNode odd = head, even = head.next, evenHead = even;
+
+        while(even != null && even.next != null) {
+            odd.next = odd.next.next;
+            even.next = even.next.next;
+            odd = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+
+        return head;
+    }
 }
