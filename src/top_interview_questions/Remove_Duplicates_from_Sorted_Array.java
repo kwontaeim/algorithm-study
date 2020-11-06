d44dcb3446e8f2af60fa6cde94b6149cfa2dab7c
Taeim Kwon <k.taemee@gmail.com>
2020-11-06 12:08:41 +0900

    .gitignore

--- src/top_interview_questions/Remove_Duplicates_from_Sorted_Array.java
+++ src/top_interview_questions/Remove_Duplicates_from_Sorted_Array.java
@@ -1,4 +1,18 @@
 package top_interview_questions;
 
 public class Remove_Duplicates_from_Sorted_Array {
+
+    /*
+
+    https://leetcode.com/explore/featured/card/top-interview-questions-easy/92/array/727/
+
+    Given a sorted array nums, remove the duplicates in-place such that each element appears only once and returns the new length.
+
+    Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+     */
+
+    public static void main(String[] args) {
+
+    }
 }
